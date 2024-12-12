5ef4dfe55037a9b4b803d7dadefac5d2dd484770
fanindya102806 <faninamani2@gmail.com>
2024-12-12 13:58:54 +0700

    api.dart

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -1,7 +1,7 @@
 class BaseUrl {
-  static String tambah = "http://192.168.100.40/biodata/create.php";
-  static String lihat = "http://192.168.100.40/biodata/details.php";
-  static String edit = "http://192.168.100.40/biodata/update.php";
-  static String hapus = "http://192.168.100.40/biodata/delete.php";
-  static String data = "http://192.168.100.40/biodata/list.php";
+  static String tambah = "http://192.168.100.40/tokofanin/create.php";
+  static String lihat = "http://192.168.100.40/tokofanin/details.php";
+  static String edit = "http://192.168.100.40/tokofanin/update.php";
+  static String hapus = "http://192.168.100.40/tokofanin/delete.php";
+  static String data = "http://192.168.100.40/tokofanin/list.php";
 }
